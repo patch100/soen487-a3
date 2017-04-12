a952ba86a13fbf5131fc334b9715c64a0a9359f3
patch100 <patch.93@gmail.com>
2017-04-12 18:23:56 -0400

    got it working

--- src/main/java/assignment3/WordCount.java
+++ src/main/java/assignment3/WordCount.java
@@ -1,8 +1,5 @@
 package assignment3;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -15,6 +12,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+import java.io.IOException;
+import java.util.*;
+
 public class WordCount {
 
     public static void main(String[] args) throws Exception {
@@ -25,11 +25,21 @@ public class WordCount {
         // parse passed arguments
         String[] files = new GenericOptionsParser(config, args).getRemainingArgs();
 
+        if(files.length != 3) {
+            System.out.println("program requires three command line arguments");
+            System.out.println("1. input text file");
+            System.out.println("2. output file");
+            System.out.println("3. Integer (will print out top N most popular words)");
+        }
+
         // input book
         Path input = new Path(files[0]);
 
         // output result file
         Path output = new Path(files[1]);
+
+        // set top N
+        config.set("N", files[2]);
 
         // prepare job
         Job job = new Job(config, "assignment3");
@@ -38,6 +48,8 @@ public class WordCount {
         job.setReducerClass(ReduceForWordCount.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+
 
         // prepare input/output/exit
         FileInputFormat.addInputPath(job, input);
@@ -52,7 +64,7 @@ public class WordCount {
             String line = value.toString();
 
             for(String token : line.split("[^\\w']+")) {
-                if(token.length() > 3) {
+                if(token.length() >= 3) {
                     word = token;
                     con.write(new Text(word), new IntWritable(1));
                 }
@@ -62,13 +74,61 @@ public class WordCount {
 
     public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable>
     {
-        public void reduce (Text word, Iterable < IntWritable > values, Context con)throws IOException, InterruptedException
-        {
+        private Map<Text, IntWritable> countMap = new HashMap<Text, IntWritable>();
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            // computes the number of occurrences of a single word
             int sum = 0;
-            for (IntWritable value : values) {
-                sum += value.get();
+            for (IntWritable val : values) {
+                sum += val.get();
             }
-            con.write(word, new IntWritable(sum));
+
+            // puts the number of occurrences of this word into the map.
+            // We need to create another Text object because the Text instance
+            // we receive is the same for all the words
+            countMap.put(new Text(key), new IntWritable(sum));
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+
+            Map<Text, IntWritable> sortedMap = sortByValue(countMap);
+
+            int counter = 0;
+
+            Configuration config = context.getConfiguration();
+            int N = Integer.parseInt(config.get("N"));
+
+            for (Text key : sortedMap.keySet()) {
+                if (counter++ == N) {
+                    break;
+                }
+                context.write(key, sortedMap.get(key));
+            }
+        }
+
+        public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map )
+        {
+            List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+
+            Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+            {
+                public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+                {
+                    return (o1.getValue()).compareTo( o2.getValue() );
+                }
+            });
+
+            Collections.reverse(list);
+
+            Map<K, V> result = new LinkedHashMap<K, V>();
+            for (Map.Entry<K, V> entry : list)
+            {
+                result.put( entry.getKey(), entry.getValue() );
+            }
+            return result;
         }
     }
 }
